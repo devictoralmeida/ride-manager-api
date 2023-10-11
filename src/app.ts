c1e26cac34cd91d3dc663caef1f60fdc274ceea1
devictoralmeida <victoremmanuelmn@gmail.com>
2023-10-11 10:21:07 -0300

    feat: add cors

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import 'express-async-errors'
 import 'dotenv/config'
 import './shared/container'
 import express from 'express'
+import cors from 'cors'
 import upload from '@config/upload'
 import swaggerUi from 'swagger-ui-express'
 import swaggerFile from './swagger.json'
@@ -13,11 +14,12 @@ const app = express()
 
 app.use(express.json())
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile)) // Nessa rota ficará nessa doc
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 app.use('/avatar', express.static(`${upload.tmpFolder}/avatar`))
 app.use('/cars', express.static(`${upload.tmpFolder}/cars`))
 
+app.use(cors())
 app.use(router)
 
 app.use(handleErrors)
